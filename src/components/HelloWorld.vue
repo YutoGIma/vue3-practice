d184b561d6bf31e276312be885b655ca4f35e5e7
GimaYuto <yurihanagima@gmail.com>
2022-07-28 21:11:13 +0900

    エラー修正と動的リストの作成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,17 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+    <List />
+  </div>
 </template>
 
 <script>
+import List from"./list.vue"
 export default {
   name: 'HelloWorld',
+  components:{List},
   props: {
     msg: String
   },
---
+++ src/components/list.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="list">
+        <h1>動的リストの作成</h1>
+        <div class="flex">
+            <input type="text" v-model="state.newFruit">
+            <button type="button" @click="addOriginFruit()">フルーツ追加</button>
+        </div>
+        <button type="button" @click="addFruit()">フルーツ増加</button>
+        <ul>
+            <li v-for="(fruit, index) of state.fruitsList" :key="index">{{fruit}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {reactive, onMounted} from "vue";
+export default {
+    setup() {
+        const state = reactive({
+            fruitsList: ["リンゴ", "パイナップル", "オレンジ", "マンゴー", "ドラゴンフルーツ"],
+            originFruits: ["リンゴ", "パイナップル", "オレンジ", "マンゴー", "ドラゴンフルーツ"],
+            newFruit: "",
+            count: 0,
+        })
+
+        function addOriginFruit(){
+            state.originFruits.push(state.newFruit)
+            console.log(state.originFruits)
+        }
+
+        function addFruit(){
+            let num = state.count % state.originFruits.length;
+            state.fruitsList.push(state.originFruits[num]);
+            state.count = state.count + 1;
+        }
+
+        return {
+            state,
+            addOriginFruit,
+            addFruit,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.flex{
+    display: flex;
+    justify-content: center;
+}
+</style>
