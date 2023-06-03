52051c4ea8124ff3c3759a207f49a0e58269f9d7
YutoGIma <yurihanagima@gmail.com>
2023-06-03 17:54:11 +0900

    リファクタリング

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="user-con">
         <div class="user-create flex">
-            <input type="text" v-model="state.createUser.name">
-            <input type="text" v-model="state.createUser.email">
-            <input type="text" v-model="state.createUser.password">
-            <textarea v-model="state.createUser.explanation" cols="0" rows="4"></textarea>
-            <input type="button" @click=" createUser() " value="送信">
+            <input type="text" v-model="newUser.name">
+            <input type="text" v-model="newUser.email">
+            <input type="text" v-model="newUser.password">
+            <textarea v-model="newUser.explanation" cols="0" rows="4"></textarea>
+            <button @click=" createUser() ">送信</button>
         </div>
         <div class="user-create-errors">
-            <P v-if="state.errors?.name">Name Error : {{ state.errors.name }}</P>
-            <P v-if="state.errors?.email">Email Error : {{ state.errors.email }}</P>
-            <P v-if="state.errors?.password">Password Error : {{ state.errors.password }}</P>
+            <P v-if="errors?.name">Name Error : {{ errors.name }}</P>
+            <P v-if="errors?.email">Email Error : {{ errors.email }}</P>
+            <P v-if="errors?.password">Password Error : {{ errors.password }}</P>
         </div>
         <div class="user-list-con">
-            <div v-for="(user, index) of state.userList" :key="index" class="user-list flex">
+            <div v-for="(user, index) of userList" :key="index" class="user-list flex">
                 <p>{{ user.id }}</p>
                 <p>{{ user.name }}</p>
                 <p>{{ user.email }}</p>
@@ -23,42 +23,42 @@
 </template>
 
 <script>
-import {reactive, onMounted} from "vue"
+import {ref, onMounted} from "vue"
 import axios from "axios"
 export default {
     setup() {
-        const state = reactive({
-            userList: [],
-            createUser: {
-                name: "",
-                email: "",
-                password: "",
-                explanation: "",
-            },
-            errors: {}
+        const userList = ref([])
+        const newUser = ref({
+            name: "",
+            email: "",
+            password: "",
+            explanation: ""
+        })
+        const errors = ref({})
+
+        onMounted(async () => {
+            try {
+                const response = await axios.get('http://localhost:3003/users')
+                userList.value = response.data
+            } catch (error) {
+                console.log(error, error.data.message)
+            }
         })
 
-        onMounted(() => {
-            axios
-            .get('http://localhost:3003/users')
-                .then((response) => {
-                    state.userList = response.data
-                })
-                .catch((error) => console.log(error, error.data.message))
-        })
-
-        function createUser(){
-            axios
-            .post('http://localhost:3003/users', state.createUser)
-            .then((response) => console.log(response.data))
-            .catch((error) => {
+        async function createUser(){
+            try {
+                const response = await axios.post('http://localhost:3003/users', newUser.value)
+                console.log(response.data)
+            } catch (error) {
                 console.log(error)
-                state.errors = error.response.data.errors
-            })
+                errors.value = error.response.data.errors
+            }
         }
 
         return {
-            state,
+            userList,
+            newUser,
+            errors,
             createUser,
         }
     },
